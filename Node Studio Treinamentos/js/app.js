3cc690f6f690d3ef7b1547ec3e1c805fa4e19d4b
Denisson Freitas <denisson_sbuky@hotmail.com>
2020-09-29 17:25:57 -0300

    aula #3 nst finalizada

--- Node Studio Treinamentos/js/app.js
+++ Node Studio Treinamentos/js/app.js
@@ -1,3 +1,11 @@
-window.alert("Olá mundo!");
+//window.alert("Olá mundo!"); //Abre uma janela de alerta o navegador!
 
-window.console.log("Eu sou o console")
+//window.console.log("Eu sou o console") // A mensagem é apresentada no console, f12 modo de desenvolvedor
+
+//window.document.getElementById("titulo")
+
+window.console.log(window.document.getElementById("titulo")) // informa o elemento buscando pelo Id
+
+console.log(window.document.getElementById("titulo").innerHTML) // informa somente o texto sem as tags html
+
+
